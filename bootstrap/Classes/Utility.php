4c29c25c080da51bde9962375eb2ac6977083ab9
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-05-13 12:54:09 +0430

    add-check-for-ajax

--- bootstrap/Classes/Utility.php
+++ bootstrap/Classes/Utility.php
@@ -48,4 +48,12 @@ class Utility
         ];
         return json_encode($message);
     }
+
+    public static function isAjaxRequest()
+    {
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ) {
+            return true;
+        }
+        return false;
+    }
 }
